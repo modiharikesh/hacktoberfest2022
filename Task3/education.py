5b8bab87bf1c0a17011da68b2be936934f9baccb
yudipi2216 <117059710+yudipi2216@users.noreply.github.com>
2022-10-31 10:16:37 +0530

    Update education.py

--- Task3/education.py
+++ Task3/education.py
@@ -1,5 +1,5 @@
 
-    ["university of São Paulo", "Information System", "2017", "2020"],
+    
     ["Chandigarh College of Engineering and Technology", "B.E. in Computer Science", "2020", "present"],
     ["university of mauritius", "Computer Science", "2018", "2021"],
     ["university of bamenda", "Nahpi", "2019", "present"],
@@ -12,6 +12,7 @@
     ["University of Bamenda", "Nahpi", "2019", "present"],
     ["University School of Information, Communication and Technology", "Bachelor of Technology", "2021", "present"],        
     ["university of bamenda", "Nahpi", "2019", "present"],
+    ["university of São Paulo", "Information System", "2017", "2020"],
     ["Board of Technical Education", "Polytechnic Diploma", "2019", "2022"],
     ["Board of Highschool and intermediate education", "Highschool", "2018", "2019"],
     ["ABV IIITM Gwalior", "B.Tech in Computer Science", "2021", "present"],
